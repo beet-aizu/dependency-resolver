df4cec7184315bdc786128552326c07e3b8039b4
beet-aizu <20266375+beet-aizu@users.noreply.github.com>
2020-11-22 17:36:24 +0900

    close #1

--- resolve.el
+++ resolve.el
@@ -1,3 +1,23 @@
+(defun paste ()
+  (interactive)
+  (goto-char (- (search-forward "//INSERT ABOVE HERE") 19))
+  (let (file buf str from to)
+    (setq file (file-truename (read-file-name "File Name: " "~/git-library/")))
+    (setq buf (find-file-noselect file))
+    (setq str
+	  (with-current-buffer buf
+	    (buffer-substring-no-properties (point-min) (point-max))))
+    (setq from (+ (string-match "//BEGIN CUT HERE" str) 17))
+    (setq to (+ (string-match "//END CUT HERE" str) 1))
+    (insert
+     (with-current-buffer buf
+       (buffer-substring-no-properties from to)))
+    (insert "\n")
+    (goto-char (point-min))
+    (replace-string "../" (file-name-directory (directory-file-name (file-name-directory file))))))
+
+(define-key global-map (kbd "C-x p") 'paste)
+
 (defun resolve ()
   (interactive)
   (setq pos (point))
